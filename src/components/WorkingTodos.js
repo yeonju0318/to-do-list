22426cee106a3a69e5ef0e04e677d691ac46a2a7
Yeonju <cuteyonju@naver.com>
2023-04-24 00:23:07 +0900

    redux폴더 리팩토링

---
+++ src/components/WorkingTodos.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function WorkingList() {
+  return <div>WorkingList</div>;
+}
--- src/redux/modules/todos.js
+++ src/redux/modules/todos.js
@@ -1,12 +1,31 @@
+//action value
 const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+const COMPLETE_TODO = "COMPLETE_TODO";
 
-const addTodo = (payload) => {
+//Action Creator
+export const addTodo = (payload) => {
   return {
     type: ADD_TODO,
     payload: payload,
   };
 };
 
+export const deleteTodo = (payload) => {
+  return {
+    type: DELETE_TODO,
+    payload: payload,
+  };
+};
+
+export const compelteTodo = (payload) => {
+  return {
+    type: COMPLETE_TODO,
+    payload: payload,
+  };
+};
+
+// Initial state
 const initialState = [
   {
     id: 1,
@@ -18,12 +37,31 @@ const initialState = [
     id: 2,
     title: "리덕스 공부하기",
     body: "리덕스 기초를 공부해봅시다.",
-    isDone: false,
+    isDone: true,
   },
 ];
 
-// const todos = (state = initialState, action) => {
-//     switch (action.type){
-//         case
-//     }
-// }
+//Reducer
+
+const todos = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [...state, action.payload];
+    case DELETE_TODO:
+      return state.filter((item) => item.id !== action.payload);
+    case COMPLETE_TODO:
+      return state.map((item) => {
+        if (item.id === action.payload) {
+          return { ...item, isDone: !item.isDone };
+        } else {
+          return item;
+        }
+      });
+
+    default:
+      return state;
+  }
+};
+
+//export default reducer
+export default todos;
